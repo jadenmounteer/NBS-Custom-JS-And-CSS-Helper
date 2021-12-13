cfa949d5fa9f90c9526db3d9b2f71a5abf8358dd
jmounteer <mounteerjaden@gmail.com>
2021-12-13 13:09:30 -0700

    Add ability to test copying directories

--- NBS-Custom-JS-And-CSS-Helper/Program.cs
+++ NBS-Custom-JS-And-CSS-Helper/Program.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+// Bring in System.IO so we can copy directories. Documentation referenced can be found here: https://docs.microsoft.com/en-us/dotnet/standard/io/how-to-copy-directories
+using System.IO;
 
 namespace NBS_Custom_JS_And_CSS_Helper
 {
     class Program
     {
+
+        // Create global variables to hold the names of the servers we will be working with
+        public static string ServerName1 = "";
+        public static string ServerName2 = "";
+        public static string ServerName3 = "";
+        public static string ServerName4 = "";
+
 
         /**
          * Show the user the options.
@@ -65,8 +74,85 @@ namespace NBS_Custom_JS_And_CSS_Helper
 
             }
 
+            // Return the number of the user's choice
             return WhatUserWantsToDo;
 
+        }
+
+
+        /**
+         *  Configures the names of the servers
+         *  
+         * */
+        static void ConfigureServers(string Environment)
+        {
+            // Tell the user what we're doing.
+            Console.WriteLine("");
+            Console.WriteLine($"Moving files to all servers for the {Environment} environment");
+            Console.WriteLine("");
+
+            // Configuer the names of the servers based off of the environment
+            switch (Environment)
+            {
+                case "Dev":
+                    /* PROODUCTION SERVER NAMES */
+                    // Note that there are a ton of backslashes because you need two to insert 1
+                    /* 
+                    ServerName1 = "\\\\10.0.5.71\\common\\custom";
+                    ServerName2 = "\\\\10.0.5.74\\common\\custom";
+                    ServerName3 = "\\\\10.0.5.76\\common\\custom";
+                    ServerName4 = "\\\\10.0.5.77\\common\\custom";
+                    */
+
+                    // For tesing, we prompt the user for the directory path
+                    Console.WriteLine("For testing purposes, please provide the first server name");
+                    ServerName1 = Console.ReadLine();
+                    Console.WriteLine($"Server 1 name: {ServerName1}");
+                    Console.WriteLine("For testing purposes, please provide the second server name");
+                    ServerName2 = Console.ReadLine();
+                    break;
+            }
+
+        }
+
+
+        /**
+         * Copies a directory. Method taken from Microsoft Docs: https://docs.microsoft.com/en-us/dotnet/standard/io/how-to-copy-directories
+         * */
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // If the destination directory doesn't exist, create it.       
+            Directory.CreateDirectory(destDirName);
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string tempPath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(tempPath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string tempPath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
+                }
+            }
         }
 
 
@@ -75,26 +161,54 @@ namespace NBS_Custom_JS_And_CSS_Helper
          */
         static void Main(string[] args)
         {
-            // Ask the user what they want to do
-            Console.WriteLine("Welcome to the NBS-Custom-JS-And-CSS-Helper");
-            Console.WriteLine("What would you like to do today?");
-            Console.WriteLine("");
-            
-            // Define a variable to track the user's input
-            string WhatUserWantsToDo = "";
+            bool ApplicationRunning = true;
 
-            // Call the ShowUserOptions method to get the user's input on how they would like to proceed
-            WhatUserWantsToDo = ShowUserOptions();
+            while(ApplicationRunning) {
 
+                // Ask the user what they want to do
+                Console.WriteLine("Welcome to the NBS-Custom-JS-And-CSS-Helper");
+                Console.WriteLine("What would you like to do today?");
+                Console.WriteLine("");
 
-            // Using the user's input, call the necessary method
-            switch (WhatUserWantsToDo)
-            {
-                case "1":
-                    Console.WriteLine("User wants to dev");
-                    break;
+                // Call the ShowUserOptions method to get the user's input on how they would like to proceed
+                string WhatUserWantsToDo = ShowUserOptions();
+
+                // Using the user's input, call the necessary method
+                switch (WhatUserWantsToDo)
+                {
+                    case "1":
+                        ConfigureServers(Environment: "Dev");
+                        //Microsoft docs example: DirectoryCopy(".", @".\temp", true);
+                        // Copy server 1 to server 2
+                        DirectoryCopy(ServerName1, ServerName2, true);
+                        // Call the method again for servers 3 and 4 here...
+                        break;
+                    case "2":
+                        Console.WriteLine("Feature not available");
+                        break;
+                    case "3":
+                        Console.WriteLine("Feature not available");
+                        break;
+                    case "4":
+                        Console.WriteLine("Feature not available");
+                        break;
+                    case "5":
+                        Console.WriteLine("Feature not available");
+                        break;
+                }
+
+                // Check if user wants to keep app running...
+                Console.WriteLine("Would you like to do something else? y/n");
+                string UserWantsToKeepAppRunning = Console.ReadLine();
+                if (UserWantsToKeepAppRunning == "n")
+                {
+                    // Close the app
+                    ApplicationRunning = false;
+                }
+                
+
             }
-
+            
 
         }
 
